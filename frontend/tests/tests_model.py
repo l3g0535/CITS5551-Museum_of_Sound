ce486d4746460fb9030a47e983f5125b18cace36
Megat Megathisham <megat_hh@hotmail.com>
2019-10-27 23:32:32 +0800

    new tests:

---
+++ frontend/tests/tests_model.py
@@ -0,0 +1,27 @@
+from mixer.backend.django import mixer
+
+class TestModels:
+
+    def test_Production_storagelocation(models.Model):
+        product = mixer.blend('frontend.Production', storage_location ='productions')
+        assert product.storage_location == 'productions'
+
+    def test_Production_ID(models.Model):
+        product = mixer.blend('frontend.Production', prod_id = 1)
+        assert product.prod_id == 1
+
+    def test_Production_Title(models.Model):
+        product = mixer.blend('frontend.Production',prod_title = 'test1')
+        assert product.prod_title == 'test1'
+
+    def test_Production_UploadTime(models.Model):
+        product = mixer.blend('frontend.Production',DateTimeField = '09/05/19T06:53:32')
+        assert product.upload_time == '09/05/19T06:53:32'
+
+    def test_Production_Description(models.Model):
+        product = mixer.blend('frontend.Production',prod_description = 'test2')
+        assert product.prod_description == 'test2'
+
+    def test_Production_IsApproved(models.Model):
+        product = mixer.blend('frontend.Production',is_approved = 'Yes')
+        assert product.is_approved == 'Yes'
--- frontend/views.py
+++ frontend/views.py
@@ -145,8 +145,7 @@ def sound_upload(request):
 @ensure_csrf_cookie
 def user_record(request):
     response_success = response = {'status': 1, 'message': ("Ok")}
-    response_fail = response = {'status': 0, 'message': (
-        "Server error, please reload the page.")}
+    response_fail = response = {'status': 0, 'message': ("Server error, please reload the page.")}
     if request.method == 'POST':
         try:
             desc = request.POST['descrip']
