0ca46ab89ac40f5d9c38b84f4f47798fb19c0d34
bryan trac <bryantrac@yaoo.ca>
2020-04-19 16:15:21 +0800

    Cleaned up styling for headers, data entry and buttons

--- static/js/location.js
+++ static/js/location.js
@@ -1,29 +1,23 @@
 var loc_field = document.getElementById("location");
 function getLocation() {
-  if(x!=null){
-      if (navigator.geolocation) {
-      console.log('processing');
-        navigator.geolocation.getCurrentPosition(showPosition);
-      } else {
-      console.log('sort of worked');
-        loc_field.innerHTML = "Geolocation is not supported by this browser.";
-      }
+  if (navigator.geolocation) {
+  console.log('processing');
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+  console.log('failed');
+    loc_field.innerHTML = "Automatic geolocation is not supported by this browser.";
   }
-  //If x is null, don't execute the code. Currently there are no plans to add an error message
 }
 
-//TODO will need to clean this section up
-function showPosition(position) {
-     let lat = position.coords.latitude;
-     let long = position.coords.longitude;
+function showPosition(pos) {
+     let lat = pos.coords.latitude;
+     let long = pos.coords.longitude;
      $("input[name='lat']").val(lat);
      $("input[name='lat']").val(long);
      let url_str = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+long+'&key=AIzaSyC4pppaQX9oFbQIj4lY0gMLDXzq1Ym-jn4'
     $.getJSON(url_str, function(data) {
           var res = '';
           for (index = 0; index < data['results'].length; ++index) {
-            //console.log(data['results'][index]['formatted_address']);
-            //console.log(data['results'][index]['types']);
             if(data['results'][index]['types'].includes('political')){
                 res = data['results'][index]['formatted_address'];
                 break;
