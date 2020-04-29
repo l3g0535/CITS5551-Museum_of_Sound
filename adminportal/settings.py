35e1ee94ed763fd99fd79d995d8d84b6fa54db16
Nicole <nicole_low93@hotmail.com>
2020-04-29 11:31:49 +0800

    sqlite db delete

--- adminportal/settings.py
+++ adminportal/settings.py
@@ -80,16 +80,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'localhost',
         'NAME': 'mosdb',
-        'PASSWORD': 'asdasd123',
+        'PASSWORD': 'a',
         'USER': 'root',
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 
 # Password validation
