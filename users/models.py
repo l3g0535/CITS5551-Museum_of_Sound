d73930b90fe792b5ed0750b21ff0ee227ee11c47
l3g0535 <l3g0535@gmail.com>
2020-05-16 10:47:37 +0800

    user profiles

--- users/models.py
+++ users/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 
 # Create your models here.
 
@@ -21,3 +24,14 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
