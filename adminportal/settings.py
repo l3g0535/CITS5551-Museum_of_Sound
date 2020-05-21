c48ef3a4decc2687bdcadb435231ba003ad70044
bryan trac <bryantrac@yaoo.ca>
2020-05-21 17:22:04 +0800

    fixed prod database reference

--- adminportal/settings.py
+++ adminportal/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'localhost',
         'NAME': 'mosdb',
-        'PASSWORD': 'a',
-        'USER': 'root',
+        'PASSWORD': 'password',
+        'USER': 'admin',
     }
 }
 
