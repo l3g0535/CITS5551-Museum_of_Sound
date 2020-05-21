df380841c9aade6fc08efb92d8d4841da24b52db
bryan trac <bryantrac@yaoo.ca>
2020-05-21 17:59:23 +0800

    fixed prod database reference

--- adminportal/settings.py
+++ adminportal/settings.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from os.path import join, dirname
 import django_heroku
-
+import dj_database_url
 
 # Needed to import API authentication codes from .env in root directory.
 dotenv_path = join(dirname(__file__), '.env')
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!(q6v&qi3-w-ktcqx_vaf6b4*#lh46u07+6+-41$0vqc9y$#$e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['http', 'sounds.arts.uwa.edu.au',
-                 '130.95.5.104', 'localhost', '127.0.0.1', '[::1]', '10.20.237.178']
+ALLOWED_HOSTS = ['http', 'https', 'sounds.arts.uwa.edu.au',
+                 '130.95.5.104', 'localhost', '127.0.0.1', '[::1]', '10.20.237.178', 'museum-of-sound.herokuapp.com']
 
 
 # Application definition
