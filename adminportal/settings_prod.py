ff53732c69f34160e3462043207adf94a626a358
bryan trac <bryantrac@yaoo.ca>
2020-05-21 17:46:05 +0800

    fixed prod database reference

--- adminportal/settings_prod.py
+++ adminportal/settings_prod.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '!(q6v&qi3-w-ktcqx_vaf6b4*#lh46u07+6+-41$0vqc9y$#$e'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = []
 
 
