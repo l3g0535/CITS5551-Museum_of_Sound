778e23b065fa7351eed00436196a81eb4e7dba1a
l3g0535 <l3g0535@gmail.com>
2020-06-02 22:34:32 +0800

    changed Debug to False

--- adminportal/settings.py
+++ adminportal/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "f8fd335675f939df3ae3e59dc53895b4f9dcbe017f002210"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['http', 'https', 'sounds.arts.uwa.edu.au',
                  '130.95.5.104', 'localhost', '127.0.0.1', '[::1]', '10.20.237.178', 'museum-of-sound.herokuapp.com', 'uwamos.herokuapp.com']
 
