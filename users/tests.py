0c460538a6db93e6dcfc0d6273103b38e0bb5295
RoyxinWang <56695638+RoyxinWang@users.noreply.github.com>
2020-06-14 23:22:43 +0800

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+from mixer.backend.django import mixer
+from user.models import Profile
+
+class TestModels:
+
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
