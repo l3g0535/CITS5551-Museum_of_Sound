4ad702b84d9186e74b4cbdd8ecbccad163e4d0fd
RoyxinWang <56695638+RoyxinWang@users.noreply.github.com>
2020-06-14 23:25:33 +0800

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -17,3 +17,321 @@ class TestModels:
     def test_Profile_Title(models.Model):
         Profile = mixer.blend('users.Profile', image = 'test1')
         assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+    
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+        def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+    
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+        def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+    
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+        def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+        
+    def test_Profile_storagelocation(models.Model):
+        Profile = mixer.blend('users.Profile', storage_location ='Profile')
+        assert Profile.storage_location == 'Profile'
+
+    def test_Profile_ID(models.Model):
+        Profile = mixer.blend('users.Profile', user = '123')
+        assert Profile.user == '123'
+
+    def test_Profile_Title(models.Model):
+        Profile = mixer.blend('users.Profile', image = 'test1')
+        assert Profile.image == 'test1'
+
+
